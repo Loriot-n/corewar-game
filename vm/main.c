513ce567f52e44aca38d0cc22e73e27c2f2bb6cb
qwebify <quentin.maire@epitech.eu>
2016-03-22 10:23:07 +0100

    added Makefile

---
+++ vm/main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for corewar in /home/qwebify/rendu/CPE/CPE_2015_corewar/vm/
+**
+** Made by maire_q
+** Login   <maire_q@epitech.eu>
+**
+** Started on  %cdate maire_q
+** Last update %udate maire_q
+*/
+
+#include "op.h"
+
+int	main(int ac, char **argv)
+{
+  
+}
