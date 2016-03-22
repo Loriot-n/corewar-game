1d465ea0fb8f40a233b1231a53d7bdf644499fea
qwebify <quentin.maire@epitech.eu>
2016-03-22 10:48:46 +0100

    created include.h

---
+++ vm/Include/include.h
@@ -0,0 +1,17 @@
+/*
+** include.h for corewar in /home/qwebify/rendu/CPE/CPE_2015_corewar/vm/Include/
+**
+** Made by maire_q
+** Login   <maire_q@epitech.eu>
+**
+** Started on  %cdate maire_q
+** Last update %udate maire_q
+*/
+
+#ifndef INCLUDE_H_
+# define INCLUDE_H_
+
+# include "op.h"
+# include <stdio.h>
+
+#endif /* INCLUDE_H_ */
