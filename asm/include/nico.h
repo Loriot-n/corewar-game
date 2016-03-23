df5dba957b2e0a3b0b67c3b8780d0bf8537cb296
Nicolas Loriot <nicolas.loriot@epitech.eu>
2016-03-23 15:21:09 +0100

    added nico.h

---
+++ asm/include/nico.h
@@ -0,0 +1,16 @@
+/*
+** nico.h for corewar in /home/nico/rendu/S02/ProgElem/CPE_2015_corewar/asm/include
+**
+** Made by Nicolas Loriot
+** Login   <loriot_n@epitech.net>
+** 
+** Started on  Tue Mar 22 20:28:55 2016 Nicolas Loriot
+**  Last update Tue Mar 22 20:28:55 2016 Nicolas Loriot
+*/
+
+#ifndef NICO_H_
+#define NICO_H_
+
+# include <stdint.h>
+
+#endif /* !NICO_H_ */
---
+++ asm/src/str_compare.c
@@ -0,0 +1,12 @@
+/*
+** str_compare.c for corewar in /home/nico/rendu/S02/ProgElem/CPE_2015_corewar/asm/src
+** 
+** Made by Nicolas Loriot
+** Login   <loriot_n@epitech.net>
+** 
+** Started on  Wed Mar 23 15:12:49 2016 Nicolas Loriot
+** Last update Wed Mar 23 15:18:31 2016 Nicolas Loriot
+*/
+
+#include "gab.h"
+#include "nico.h"
