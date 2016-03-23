268f689b122f5a2c9f9b9e181c6e571469aeb4b0
Nicolas Loriot <nicolas.loriot@epitech.eu>
2016-03-23 16:01:19 +0100

    strcmp

--- asm/src/str_compare.c
+++ asm/src/str_compare.c
@@ -5,8 +5,38 @@
 ** Login   <loriot_n@epitech.net>
 ** 
 ** Started on  Wed Mar 23 15:12:49 2016 Nicolas Loriot
-** Last update Wed Mar 23 15:18:31 2016 Nicolas Loriot
+** Last update Wed Mar 23 16:01:06 2016 Nicolas Loriot
 */
 
 #include "gab.h"
 #include "nico.h"
+
+int	my_strcmp(char *s1, char *s2)
+{
+  while ((*s1 != '\0') && (*s2 != '\0'))
+    {
+      if (*s1 != *s2)
+	return (-1);
+      s1++;
+      s2++;
+    }
+  if ((*s1 == '\0') && (*s2 == '\0'))
+    return (0);
+  if (*s2 == '\0')
+    return (1);
+  return (1);
+}
+
+char	*my_strcpy(char *dest, char *src)
+{
+  int	i;
+
+  i = 0;
+  while (src[i] != '\0')
+    {
+      dest[i] = src[i];
+      i = i + 1;
+    }
+  dest[i] = '\0';
+  return (dest);
+}
