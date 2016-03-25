07d7547e34187075e3aaed9f8bed047e21a66839
Nicolas Loriot <nicolas.loriot@epitech.eu>
2016-03-25 13:00:08 +0100

    ntm git

--- asm/src/params_type.c
+++ asm/src/params_type.c
@@ -7,26 +7,3 @@
 ** Started on  Thu Mar 24 03:22:20 2016 Nicolas Loriot
 ** Last update Fri Mar 25 11:54:28 2016 Nicolas Loriot
 */
-
-#include "nico.h"
-#include "gab.h"
-
-unsigned char	set_param_byte(int det)
-{
-  unsigned char	n;
-  int		a;
-  int		b;
-  int		c;
-
-  n = 0;
-  a = det / 100;
-  b = (det % 100) / 10;
-  c = det % 10;
-  (a == 1) ? (BIT_CLEAR(n, 7), BIT_SET(n, 6)) : (a == 2) ? (BIT_SET(n, 7)) :
-      (a == 3) ? (BIT_SET(n, 7), BIT_SET(n, 6)) : (0);
-  (b == 1) ? (BIT_SET(n, 4)) : (b == 2) ? (BIT_SET(n, 5)) :
-      (b == 3) ? (BIT_SET(n, 5), BIT_SET(n, 4)) : (0);
-  (c == 1) ? (BIT_SET(n, 2)) : (c == 2) ? (BIT_SET(n, 3)) :
-      (c == 3) ? (BIT_SET(n, 3), BIT_SET(n, 2)) : (0);
-  return (n);
-}
