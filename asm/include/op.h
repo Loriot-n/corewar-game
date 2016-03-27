98039ed81b93e60335c650149c99a69fec90e06e
Stanislas Cuenat <stanislas.cuenat@epitech.eu>
2016-03-27 23:29:55 +0200

    ok

--- asm/include/op.h
+++ asm/include/op.h
@@ -5,15 +5,15 @@
 ** Login   <loriot_n@epitech.net>
 **
 ** Started on  Fri Mar 25 16:53:44 2016 Nicolas Loriot
-** Last update Sat Mar 26 13:51:18 2016 Nicolas Loriot
+** Last update Sun Mar 27 23:28:51 2016 CUENAT
 */
 
 #ifndef _OP_H_
 # define _OP_H_
 
-# define MEM_SIZE                (6*1024)
-# define IDX_MOD                 512   /* modulo de l'index < */
-# define MAX_ARGS_NUMBER         4    /* this may not be changed 2^*IND_SIZE */
+# define MEM_SIZE                (6 * 1024)
+# define IDX_MOD                 512
+# define MAX_ARGS_NUMBER         4
 # define COMMENT_CHAR            '#'
 # define LABEL_CHAR              ':'
 # define DIRECT_CHAR             '%'
@@ -24,14 +24,14 @@
 typedef char    args_type_t;
 
 /*
- ** regs
- */
+** regs
+*/
 
-# define REG_NUMBER      16              /* r1 <--> rx */
-# define T_REG           1       /* registre */
-# define T_DIR           2       /* directe  (ld  #1, r1  met 1 dans r1) */
+# define REG_NUMBER      16
+# define T_REG           1
+# define T_DIR           2
 # define T_IND           4
-# define T_LAB           8       /* LABEL */
+# define T_LAB           8
 
 typedef struct	op_s
 {
@@ -44,25 +44,25 @@ typedef struct	op_s
 }		op_t;
 
 /*
- ** size
- */
+** size
+*/
 
-# define IND_SIZE        2               /* en octet */
-# define REG_SIZE        4               /* en octet */
-# define DIR_SIZE        REG_SIZE        /* en octet */
+# define IND_SIZE        2
+# define REG_SIZE        4
+# define DIR_SIZE        REG_SIZE
 
 /*
- ** op_tab
- */
+** op_tab
+*/
 
 extern  op_t    op_tab[];
 
 /*
- ** header
- */
+** header
+*/
 # define PROG_NAME_LENGTH        128
 # define COMMENT_LENGTH          2048
-# define COREWAR_EXEC_MAGIC      0xea83f3        /* why not */
+# define COREWAR_EXEC_MAGIC      0xea83f3
 
 typedef struct header_s
 {
@@ -73,10 +73,10 @@ typedef struct header_s
 }		header_t;
 
 /*
- ** live
- */
+** live
+*/
 
-# define CYCLE_TO_DIE    1536  /* nombre de cycle pour etre d\'eclarer mort */
+# define CYCLE_TO_DIE    1536
 # define CYCLE_DELTA     5
 # define NBR_LIVE        40
 
